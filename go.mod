6a4193eb22e929432a486b8f6accde8b97e6f80c
AppliNH <thomas.martindev@gmail.com>
2020-01-07 18:48:06 +0100

    can now display an article

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go-serverlessRss
 go 1.13
 
 require (
+	github.com/766b/go-outliner v0.0.0-20180511142203-fc6edecdadd7 // indirect
 	github.com/PuerkitoBio/goquery v1.5.0 // indirect
 	github.com/gorilla/mux v1.7.3
 	github.com/mmcdole/gofeed v1.0.0-beta2
--- main.go
+++ main.go
@@ -17,11 +17,32 @@ func Rewriter(h http.Handler) http.Handler {
 		//Simple URL rewriter. Rewrite if it's started with API path
 		pathReq := r.RequestURI
 
-		if strings.HasPrefix(pathReq, "/api/v1/rss/") { //&& strings.HasSuffix(pathReq,"/item/")
-			//Use url.QueryEscape for pre go1.8
-			pe := url.PathEscape(strings.TrimLeft(pathReq, "/api/v1/rss/"))
-			r.URL.Path = "/api/v1/rss/" + pe
-			r.URL.RawQuery = ""
+		if strings.Contains(pathReq, `/item/`) {
+			if strings.HasPrefix(pathReq, "/api/v1/rss/") {
+				fmt.Println("two")
+
+				s1 := strings.TrimLeft(pathReq, "/api/v1/rss/")
+				i := strings.Index(s1, "/item/")
+				uri := s1[:i]
+				s2 := s1[i:]
+				fmt.Println("s1 : ", s1)
+				fmt.Println("s2 : ", s2)
+				fmt.Println("uri : ", uri)
+
+				pe := url.PathEscape(uri)
+
+				r.URL.Path = "/api/v1/rss/" + pe + s2
+
+				r.URL.RawQuery = ""
+			}
+		}
+		if !strings.Contains(pathReq, `/item/`) {
+			if strings.HasPrefix(pathReq, "/api/v1/rss/") && !strings.HasSuffix(pathReq, "/item/") {
+				fmt.Println("many")
+				pe := url.PathEscape(strings.TrimLeft(pathReq, "/api/v1/rss/"))
+				r.URL.Path = "/api/v1/rss/" + pe
+				r.URL.RawQuery = ""
+			}
 		}
 
 		h.ServeHTTP(w, r)
@@ -84,6 +105,8 @@ func params(w http.ResponseWriter, r *http.Request) {
 }
 
 func rssArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("rssArticles")
+
 	pathParams := mux.Vars(r)
 	rssUri := pathParams["rss"]
 
@@ -101,13 +124,37 @@ func rssArticles(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func rssArticle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("rssArticle")
+
+	pathParams := mux.Vars(r)
+	rssUri := pathParams["rss"]
+	item, erro := strconv.Atoi(pathParams["item"])
+
+	fmt.Println(erro)
+	rss, err := url.PathUnescape(rssUri)
+
+	fp := gofeed.NewParser()
+	feed, _ := fp.ParseURL(rss)
+	fmt.Println(err)
+
+	article := feed.Items[item]
+
+	w.Write([]byte(fmt.Sprintf(`| %s |
+	 
+	%s
+	
+	`, article.Title, article.Description)))
+
+}
+
 func main() {
 	r := mux.NewRouter()
 
 	api := r.PathPrefix("/api/v1").Subrouter()
 	api.SkipClean(true)
 	api.HandleFunc("/rss/{rss}", rssArticles)
-
+	api.HandleFunc("/rss/{rss}/item/{item}", rssArticle)
 	api.HandleFunc("", get).Methods(http.MethodGet)
 	api.HandleFunc("", post).Methods(http.MethodPost)
 	api.HandleFunc("", put).Methods(http.MethodPut)
@@ -115,5 +162,5 @@ func main() {
 
 	api.HandleFunc("/user/{userID}/comment/{commentID}", params).Methods(http.MethodGet)
 
-	log.Fatal(http.ListenAndServe(":8082", Rewriter(r)))
+	log.Fatal(http.ListenAndServe(":8080", Rewriter(r)))
 }
